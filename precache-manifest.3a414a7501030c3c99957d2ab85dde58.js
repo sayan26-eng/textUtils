f40a12953ec5e71e3412dcbf964cfa202c8ef7ee
sayan26-eng <www.sansfirefox@gmail.com>
2023-01-08 22:41:10 +0530

    Updates

---
+++ precache-manifest.3a414a7501030c3c99957d2ab85dde58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8780a93291c079841c3",
+    "url": "/textUtils/static/css/main.0ce5ad08.chunk.css"
+  },
+  {
+    "revision": "c8780a93291c079841c3",
+    "url": "/textUtils/static/js/main.c8780a93.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/textUtils/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/textUtils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "2eeb5852bcf23a524757",
+    "url": "/textUtils/static/js/runtime~main.2eeb5852.js"
+  },
+  {
+    "revision": "fb5af5d88d64592c00e6e11a3b5f20cb",
+    "url": "/textUtils/index.html"
+  }
+];
